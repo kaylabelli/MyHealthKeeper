83595acda4c19d34892517ae5f81c9a2449818ad
Amran <amranuddin1294@gmail.com>
2019-03-12 14:19:24 -0400

    Populated calendar with numbers and added next/previous month function

--- healthapp/calendarViewController.swift
+++ healthapp/calendarViewController.swift
@@ -8,6 +8,14 @@
 import Foundation
 import UIKit
 
+let date = Date()
+let currentCalendar = Calendar.current
+
+let day = currentCalendar.component(.day, from: date)
+let weekday = currentCalendar.component(.weekday, from: date)
+var month = currentCalendar.component(.month, from: date) - 1
+var year = currentCalendar.component(.year, from: date)
+
 class calendarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
 {
     private let sectionInsets = UIEdgeInsets(top: 0.0,
@@ -15,28 +23,71 @@ class calendarViewController: UIViewController, UICollectionViewDelegate, UIColl
                                              bottom: 0.0,
                                              right: 13.0)
     
+    let Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    let numOfDays = [38, 35, 38, 37, 38, 37, 38, 37, 38, 37, 38, 37]
+    
     @IBOutlet weak var calendar: UICollectionView!
     @IBOutlet weak var currentCalendarLabel: UILabel!
     
-    
-    @IBAction func previousMonth(_ sender: Any) {
-    }
-    
-    @IBAction func nextMonth(_ sender: Any) {
-    }
+    var currentMonth = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentMonth = Months[month]
+        
+        currentCalendarLabel.text = "\(currentMonth) \(year)"
+        
+    }
+    
+    
+    @IBAction func previousMonth(_ sender: Any) {
+        switch currentMonth {
+        case "January":
+            month = 11
+            year -= 1
+            currentMonth = Months[month]
+            currentCalendarLabel.text = "\(currentMonth) \(year)"
+            calendar.reloadData()
+        default:
+            month -= 1
+            currentMonth = Months[month]
+            currentCalendarLabel.text = "\(currentMonth) \(year)"
+            calendar.reloadData()
+        }
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+        switch currentMonth {
+        case "December":
+            month = 0
+            year += 1
+            currentMonth = Months[month]
+            currentCalendarLabel.text = "\(currentMonth) \(year)"
+            calendar.reloadData()
+        default:
+            month += 1
+            currentMonth = Months[month]
+            currentCalendarLabel.text = "\(currentMonth) \(year)"
+            calendar.reloadData()
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 31
+        return numOfDays[month]
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Calendar", for: indexPath) as! calendarCollectionViewCell
         
-        cell.Date.text = "Day"
+        switch indexPath.row
+        {
+        case 0...6:
+            cell.Date.text = dayName[indexPath.row]
+        default:
+            cell.Date.text = String(indexPath.row - 6)
+        }
         
         return cell
     }
